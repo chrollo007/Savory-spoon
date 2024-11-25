a8eb2698b008cf2ae995490b9052b28f7c8530f6
Aman Rajbhar <104845206+chrollo007@users.noreply.github.com>
2024-11-26 00:28:16 +0530

    Update Profile.jsx

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {motion} from "framer-motion";
-import me from "../../assets/yummy.jpg"
+import me from "../../assets/yummy.jpg";
 import { Link } from "react-router-dom";
 const Profile=()=>{
     const options={
